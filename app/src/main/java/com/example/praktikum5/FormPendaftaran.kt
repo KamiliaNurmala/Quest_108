1f798e03de8cf57855a7643171ee99060ca6ee0f
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-01 03:30:20 +0700

    Menambah package dan imports

--- app/src/main/java/com/example/praktikum5/FormPendaftaran.kt
+++ app/src/main/java/com/example/praktikum5/FormPendaftaran.kt
@@ -1,1 +1,16 @@
 package com.example.praktikum5
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
